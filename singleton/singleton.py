7d91449cd651dd70ea43eb4c88d41c55e0cfcd42
alexs <alexsquiroz054@gmail.com>
2022-10-26 09:42:36 -0500

    creacion de comentarios en singleton

--- singleton/singleton.py
+++ singleton/singleton.py
@@ -1,9 +1,9 @@
 
-def singleton(cls):
+def singleton(cls):             #Creacion de Decorador para clases
     
     instances = dict()
     
-    def wrap(*args, **kwards):
+    def wrap(*args, **kwards):          #Creacion de listado de argumentos y diccionario de argumentos  
         if cls not in instances:
             instances [cls] = cls(*args, **kwards)
 
@@ -11,21 +11,19 @@ def singleton(cls):
 
     return wrap
 
-@singleton
+@singleton                  #Decorador
 class sena(object):
     def __init__(self, username):
         self.username = username
 
-if __name__ == '__main__':
+if __name__ == '__main__':                 #Creador de objetos
 
     sena1 = sena('alexs')
     sena2 = sena('cesar')
     sena2 = sena('brandon')
     sena2 = sena('oscar')
 
-
-
-    print(sena1 is sena2)
+    print(sena1 is sena2)       #Reconocer si los objetos son verdaderos 
 
         
 
